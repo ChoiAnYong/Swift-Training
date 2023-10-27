70c92ce3dfda88953b289b2d691140b510585d7d
ChoiAnyong <dksdyd78@naver.com>
2023-10-27 19:16:35 +0900

    First

---
+++ Half.swift
@@ -0,0 +1,18 @@
+//
+//  Half.swift
+//  FastSwiftBasic
+//
+//  Created by 최안용 on 10/8/23.
+//
+
+import SwiftUI
+
+struct Half: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Half()
+}
